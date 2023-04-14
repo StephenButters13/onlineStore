015f0a0dc4f6df0a4be67ddf44275c5bde2269ae
Stephen Butters <srbutters05@gmail.com>
2023-04-13 20:01:19 -0400

    updated data and deleted images

--- database/schema.sql
+++ database/schema.sql
@@ -1,11 +1,13 @@
 CREATE TABLE inventory (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     item_name VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
     info VARCHAR(255) NOT NULL,
     price DECIMAL(10,2) NOT NULL,
     stock INTEGER NOT NULL,
     image_url VARCHAR(255) NOT NULL,
-    category VARCHAR(255) NOT NULL
+    genre VARCHAR(255) NOT NULL,
+    book_pagecount INTEGER NOT NULL
 );
 
 CREATE TABLE users (
@@ -13,7 +15,9 @@ CREATE TABLE users (
     password_hash VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL
+    last_name VARCHAR(255) NOT NULL,
+    user_address VARCHAR(255) NOT NULL,
+    user_payment VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE sales (
--- database/startingData.sql
+++ database/startingData.sql
@@ -1,5 +1,5 @@
 INSERT into `users` (`username`, `password_hash`, `email`, `first_name`, `last_name`)
-VALUES ('aturing', 'b93727798b520dc10d145b53909c061f082ff14cd5f8cb4ab24c3b71bfa57d7e12e1296029be74c06a0d91ba32756f9fc978047fbe7232be67f94dfc1de9ced9', 'alan@enigma.com', 'Alan', 'Turing');
+VALUES ('sbutters', 'b93727798b520dc10d145b53909c061f082ff14cd5f8cb4ab24c3b71bfa57d7e12e1296029be74c06a0d91ba32756f9fc978047fbe7232be67f94dfc1de9ced9', 'stephenbutters@gmail.com', 'Stephen', 'Butters');
 
 INSERT into `users` (`username`, `password_hash`, `email`, `first_name`, `last_name`)
 VALUES ('dritchie', '67aff785bd17ac24448d491926ff7aadd8fa75e51a2f7a9bfc31889bad0adcd2989061a27ccd9eff9e5e31f2bc14b5c193727e116dc8dc48259acb3919171cd4', 'dennis@unix.com', 'Dennis', 'Ritchie');
@@ -10,14 +10,29 @@ VALUES ('llamport', '9171d14954eeda4e70777c23d98e349818125cdaeb884ff97ebf8cc0a9c
 INSERT into `users` (`username`, `password_hash`, `email`, `first_name`, `last_name`)
 VALUES ('bliskov', '1e4b9ae956cad1385cfa6fffd8323dd16c3fe18c54e6447e49bddef2138d042e84e1505a541c6ef19a5026e684b2559efd366145870a0a8d4d4173c0877f6cd2', 'barbara@thor.com', 'Barbara', 'Liskov');
 
-INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
-VALUES ('Apples', 'An edible cultivation of the Malus genus.', 2.00, 100, 'static/images/apple.jpeg', 'Fruit');
+INSERT into `inventory` (`item_name`, 'author', `info`, `price`, `stock`, `image_url`, `genre`, 'book_pagecount')
+VALUES ('The Plot', 'Jean Korelitz', "Finch Bonner a young novelist writes a book about a close student's death.", 17.00, 100, 'static/images/apple.jpeg', 'Mysterty',322);
 
-INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
-VALUES ('Bananas', 'A long curved fruit which grows in clusters and has soft pulpy flesh and yellow skin when ripe.', 1.00, 100, 'static/images/banana.jpeg', 'Fruit');
+INSERT into `inventory` (`item_name`, 'author', `info`, `price`, `stock`, `image_url`, `genre`, 'book_pagecount')
+VALUES ('The Fish', 'Joanne Stubbs', "A few decades into the twenty-first century in New Zealand, Cathy must deal with sea creatures invading the land.", 16.00, 100, 'static/images/banana.jpeg', 'Science Fiction',253);
 
-INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
-VALUES ('Mangos', 'The best fruit on the planet.', 4.00, 100, 'static/images/mango.jpeg', 'Fruit');
+INSERT into `inventory` (`item_name`, 'author', `info`, `price`, `stock`, `image_url`, `genre`, 'book_pagecount')
+VALUES ('Snow', 'John Banville', "Detective St. John Strafford has been summoned to investigate a murdered priest.", 14.00, 100, 'static/images/mango.jpeg', 'Mystery', 299);
+
+INSERT into `inventory` (`item_name`, 'author', `info`, `price`, `stock`, `image_url`, `genre`, 'book_pagecount')
+VALUES ('Moth', 'Melody Razak', "Overnight after the British Raj is fractured, Alma and her family become torn apart and now they must find new increasingly desperate ways to survive.", 19.00, 100, 'static/images/mango.jpeg', 'Historical Fiction', 344);
+
+INSERT into `inventory` (`item_name`, 'author', `info`, `price`, `stock`, `image_url`, `genre`, 'book_pagecount')
+VALUES ('Hungry Ghosts', 'Kevin Hosein', 'Due to a disappearance of a rich fellow, Dalton, the rich and poor lives become entwined and change in shocking ways forever.', 18.00, 100, 'static/images/mango.jpeg', 'Psycological Fiction', 326);
+
+INSERT into `inventory` (`item_name`, 'author', `info`, `price`, `stock`, `image_url`, `genre`, 'book_pagecount')
+VALUES ('The Midnight Library', 'Matt Haig', 'Nora Seed stumbles upon the Midnight Library, which gives her a chance to live out a different life.', 16.00, 100, 'static/images/mango.jpeg', 'Science Fiction', 288);
+
+INSERT into `inventory` (`item_name`, 'author', `info`, `price`, `stock`, `image_url`, `genre`, 'book_pagecount')
+VALUES ('Shards of Earth', 'Adrian Tchaikovsky', 'Indris and his crew look for answers about the possibly returning Architects while they get hunted by gangs, cults and governments.', 24.00, 100, 'static/images/mango.jpeg', 'Science Fiction', 548);
+
+INSERT into `inventory` (`item_name`, 'author', `info`, `price`, `stock`, `image_url`, `genre`, 'book_pagecount')
+VALUES ('Daisy Darker', 'Alice Feeney', 'Daisy and her family get cut off from the rest of the world for 8 hours when the tide comes in.', 18.00, 100, 'static/images/mango.jpeg', 'Mystery', 335);
 
 INSERT into `sales` (`transaction_id`, `username`, `item_id`, `quantity`, `sale_date`, `cost`)
 VALUES ('1', 'aturing', '1', 10, '2022-12-21 7:30:30', 5.50);
