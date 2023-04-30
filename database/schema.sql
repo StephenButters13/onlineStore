a8c514b32cc03365317a70c1dca180371c89b06e
root <root@LAPTOP-V70J0FE7>
2023-04-29 22:10:59 -0400

    fixed the issue to where the app.py would not run

--- database/schema.sql
+++ database/schema.sql
@@ -15,9 +15,7 @@ CREATE TABLE users (
     password_hash VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    user_address VARCHAR(255) NOT NULL,
-    user_payment VARCHAR(255) NOT NULL
+    last_name VARCHAR(255) NOT NULL    
 );
 
 CREATE TABLE sales (
